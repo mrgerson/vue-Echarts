7ec5ae7700ab7bc397eccd5dc43671ed6a38e75d
Gerson Calvo <gedacaba@gmail.com>
2022-08-17 11:47:46 -0500

    trabajando con vue-echarts

--- src/main.js
+++ src/main.js
@@ -3,10 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+    faTwitter,
+    faFacebook,
+    faStackOverflow,
+    faGithub,
+    faFolder,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBars, faTwitter, faFacebook, faStackOverflow, faGithub);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
